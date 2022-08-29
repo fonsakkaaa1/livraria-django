d9050218e6f0095f8fa27050eb3ad16036adb064
fonsakkaaa1 <jirenmlk@gmail.com>
2022-08-29 16:34:54 -0300

    terceiro commit

--- core/views.py
+++ core/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from core.models import Categoria, Editora, Autor, Livro
+from core.serializers import CategoriaSerializer, EditoraSerializer, AutorSerializer,LivroSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+class EditoraViewSet(ModelViewSet):
+    queryset = Editora.objects.all()
+    serializer_class = CategoriaSerializer
+
+class AutorViewSet(ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = CategoriaSerializer
+
+class LivroViewSet(ModelViewSet):
+    queryset = Livro.objects.all()
+    serializer_class = CategoriaSerializer
+
