fec4598d15e2ad56b7fe0947bb7694f9e9a69853
fonsakkaaa1 <jirenmlk@gmail.com>
2022-11-21 16:23:32 -0300

    aula 15 e 16

--- core/serializers.py
+++ core/serializers.py
@@ -4,13 +4,16 @@ from core.models import Categoria, Editora, Autor, Livro
 
 class CategoriaSerializer(ModelSerializer):
     class Meta:
-        model = Categoria
+        model=Categoria
         fields = "__all__"
 
 class EditoraSerializer(ModelSerializer):
     class Meta:
         model = Editora
         fields = "__all__"
+
+
+
 
 class AutorSerializer(ModelSerializer):
     class Meta:
